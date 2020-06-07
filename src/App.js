dae7a02d46f621f911fb45421b590af73c80e811
Adam Miszczak <adammiszczak1@gmail.com>
2020-06-08 00:53:46 +0200

    initial preparation, instaling react router, bootstrap, sass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function MainNav() {
+  return (
+    <React.Fragment>
+      <Navbar collapseOnSelect fixed="top" expand="lg" bg="dark" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/"><Link className="nav-link" to="/">Home page</Link></Nav.Link>
+            <Nav.Link href="/about"><Link className="nav-link" to="/about">About project</Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </React.Fragment>
+  );
+}
+
+function Home() {
+  return (
+        <Container fluid={true} className="d-flex vh-100 align-items-center justify-content-center">
+        <Row lg={12}  className="text-center">
+          <Col className="text-center">
+          <h1>React ToDo App with hooks</h1>
+          </Col>
+        </Row>
+      </Container>
+  );
+};
+
+function About() {
+  return (
+    <Container fluid={true} className="d-flex vh-100 align-items-center justify-content-center">
+      <Row lg={12}  className="text-center">
+        <Col className="text-center">
+        <h1>About Page</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router basename="/react-hooks-todo-app/">
+        <MainNav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </Router>
     </div>
   );
 }
